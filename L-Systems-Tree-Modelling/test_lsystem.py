5c11fae0cd68ac4f60afb39ae684e88dcbfb9039
jeanneinchauspe <jeanne.inchauspe@gmail.com>
2021-10-28 11:45:01 +0100

    changes to tesst_lsystem

--- L-Systems-Tree-Modelling/test_lsystem.py
+++ L-Systems-Tree-Modelling/test_lsystem.py
@@ -5,30 +5,33 @@ class LSystemTest(unittest.TestCase):
     """
     Tests the :class:`argsChecker` class.
     """
-    def test_check_iter(self):
+    def test_addToDict(self):
+        axiom='F'
+        updateStr="FF-[-F+F+F]+[+F-F-F]"
+        testLS = LSystem()
+        testLS.addToDict(axiom, updateStr)
+        assert testLS[axiom] == "FF-[-F+F+F]+[+F-F-F]"
 
-        with self.assertRaises(TypeError):
-            argsChecker.checkIter(0)
 
-    def test_checkAngle(self):
+    # def test_checkAngle(self):
 
-        with self.assertRaises(TypeError):
-            argsChecker.checkAngle(-45)
+    #     with self.assertRaises(TypeError):
+    #         argsChecker.checkAngle(-45)
 
-    def test_checkAxiom(self):
+    # def test_checkAxiom(self):
 
-        with self.assertRaises(TypeError):
-            argsChecker.checkAxiom('1')
+    #     with self.assertRaises(TypeError):
+    #         argsChecker.checkAxiom('1')
     
-    def test_checkUpdateStrLetters(self):
+    # def test_checkUpdateStrLetters(self):
 
-        with self.assertRaises(TypeError):
-            argsChecker.checkUpdateStrLetters("?")
+    #     with self.assertRaises(TypeError):
+    #         argsChecker.checkUpdateStrLetters("?")
     
-    def test_checkUpdateStrRecursion(self):
+    # def test_checkUpdateStrRecursion(self):
 
-        with self.assertRaises(TypeError):
-            argsChecker.checkUpdateStrRecursion(123)
+    #     with self.assertRaises(TypeError):
+    #         argsChecker.checkUpdateStrRecursion(123)
 
 
 
