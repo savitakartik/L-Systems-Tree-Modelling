205b5f1b4667779d4bfa1835e318dbe83d99b4a9
jeanneinchauspe <jeanne.inchauspe@gmail.com>
2021-10-28 15:29:18 +0100

    test lsystem shortened

--- L-Systems-Tree-Modelling/test_lsystem.py
+++ L-Systems-Tree-Modelling/test_lsystem.py
@@ -10,32 +10,4 @@ class LSystemTest(unittest.TestCase):
         updateStr="FF-[-F+F+F]+[+F-F-F]"
         testLS = LSystem()
         assert testLS.addToDict(axiom, updateStr) == "FF-[-F+F+F]+[+F-F-F]"
-    def test_addToDict(self):
-        axiom='F'
-        updateStr="FF-[-F+F+F]+[+F-F-F]"
-        testLS = LSystem()
-        assert testLS.addToDict(axiom, updateStr) == "FF-[-F+F+F]+[+F-F-F]"
-
-
-    # def test_checkAngle(self):
-
-    #     with self.assertRaises(TypeError):
-    #         argsChecker.checkAngle(-45)
-
-    # def test_checkAxiom(self):
-
-    #     with self.assertRaises(TypeError):
-    #         argsChecker.checkAxiom('1')
-    
-    # def test_checkUpdateStrLetters(self):
-
-    #     with self.assertRaises(TypeError):
-    #         argsChecker.checkUpdateStrLetters("?")
-    
-    # def test_checkUpdateStrRecursion(self):
-
-    #     with self.assertRaises(TypeError):
-    #         argsChecker.checkUpdateStrRecursion(123)
-
-
 
