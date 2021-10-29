dadbcc1a43ff36b4b693ffc836970ff56472da44
jeanneinchauspe <jeanne.inchauspe@gmail.com>
2021-10-29 15:00:53 +0100

    gui test modif

--- L-Systems-Tree-Modelling/GUI_test.py
+++ L-Systems-Tree-Modelling/GUI_test.py
@@ -1,17 +1,19 @@
-from PyQt5 import *
 import unittest
+from PyQt5 import *
 import sys
 from GUI import Window
 from PyQt5.QtGui import *
 from PyQt5.QtTest import *
 from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
 
 class GUITest(unittest.TestCase):
     def test_defaults(self):
-        self.assertEqual(self.frulebox.text(), '"F[+F]F[-F][F]"' )
-        self.assertEqual(self.check1.isChecked(), True)
-        self.assertEqual(self.numiterbox.value(), 5)
-        self.assertEqual(self.anglespinbox.value(), 20)
-        self.assertEqual(self.colourschemedd.text(), "natural")
-        self.assertEqual(self.colourdistdd.text(), "sequential")
-        self.assertEqual(self.ltslider.value(), 2)
+        window = Window(QWidget)
+        self.assertEqual(window.frulebox.text(), '"F[+F]F[-F][F]"' )
+        self.assertEqual(window.check1.isChecked(), True)
+        self.assertEqual(window.numiterbox.value(), 5)
+        self.assertEqual(window.anglespinbox.value(), 20)
+        self.assertEqual(window.colourschemedd.text(), "natural")
+        self.assertEqual(window.colourdistdd.text(), "sequential")
+        self.assertEqual(window.ltslider.value(), 2)
