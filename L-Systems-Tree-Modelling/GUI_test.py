116c5f33f903b1b6858fce3db34439d2a9553353
jeanneinchauspe <jeanne.inchauspe@gmail.com>
2021-10-29 15:28:38 +0100

    gui test updated

--- L-Systems-Tree-Modelling/GUI_test.py
+++ L-Systems-Tree-Modelling/GUI_test.py
@@ -1,15 +1,22 @@
 import unittest
 from PyQt5 import *
 import sys
+import GUI
 from GUI import Window
 from PyQt5.QtGui import QApplication
 from PyQt5.QtTest import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
+app = QApplication(sys.argv)
 class GUITest(unittest.TestCase):
+    '''Test the margarita mixer GUI'''
+    def setUp(self):
+        '''Create the GUI'''
+        self.form = GUI.Window()
+        
     def test_defaults(self):
-        app = QApplication(sys.argv)
+ 
         window = Window()
         window.show()
         self.assertEqual(window.frulebox.text(), '"F[+F]F[-F][F]"' )
@@ -20,3 +27,6 @@ class GUITest(unittest.TestCase):
         self.assertEqual(window.colourdistdd.text(), "sequential")
         self.assertEqual(window.ltslider.value(), 2)
         sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    unittest.main()
