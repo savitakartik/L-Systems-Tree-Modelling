c9099f9bc9a7c6edc14efa33e27ca745df76f973
jeanneinchauspe <jeanne.inchauspe@gmail.com>
2021-10-29 11:51:33 +0100

    tutle2D test

--- L-Systems-Tree-Modelling/test_Turtle2D_Interpreter.py
+++ L-Systems-Tree-Modelling/test_Turtle2D_Interpreter.py
@@ -1,5 +1,6 @@
 import unittest
-from Turtle2D import Turtle2D
+import numpy as np
+from Turtle2D_interpretation import Turtle2D
 
 class Turtle2DTest(unittest.TestCase):
     """
@@ -8,7 +9,7 @@ class Turtle2DTest(unittest.TestCase):
     def test_show_tree(self):
         turtle=Turtle2D(p0=np.array([250,0]),o0=np.array([0,1]),std_d=10,std_delta=29.5)
         next_position=turtle.next_position()
-        drawer=Tree_drawing_2D(turtle,(50,50),color_scheme='natural',color_type='sequential')
+        drawer=Tree_drawing_2D(turtle,(100,100),color_scheme='natural',color_type='sequential')
         test_string='[F[+F][-F[-F]F]F[+F][-F]]'
         drawer.draw_tree(test_string)
         with self.assertWarns(Warning):
