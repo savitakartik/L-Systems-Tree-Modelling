5bf5409e71820d062bd63e07a6fc63b58a26348b
jeanneinchauspe <jeanne.inchauspe@gmail.com>
2021-10-29 15:16:48 +0100

    gui test

--- L-Systems-Tree-Modelling/GUI_test.py
+++ L-Systems-Tree-Modelling/GUI_test.py
@@ -2,14 +2,16 @@ import unittest
 from PyQt5 import *
 import sys
 from GUI import Window
-from PyQt5.QtGui import *
+from PyQt5.QtGui import QApplication
 from PyQt5.QtTest import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
 class GUITest(unittest.TestCase):
     def test_defaults(self):
-        window = Window(QWidget)
+        app = QApplication(sys.argv)
+        window = Window()
+        window.show()
         self.assertEqual(window.frulebox.text(), '"F[+F]F[-F][F]"' )
         self.assertEqual(window.check1.isChecked(), True)
         self.assertEqual(window.numiterbox.value(), 5)
@@ -17,3 +19,4 @@ class GUITest(unittest.TestCase):
         self.assertEqual(window.colourschemedd.text(), "natural")
         self.assertEqual(window.colourdistdd.text(), "sequential")
         self.assertEqual(window.ltslider.value(), 2)
+        sys.exit(app.exec_())
