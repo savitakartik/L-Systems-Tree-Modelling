f2d1e24e1127e5a1dafda797e1f4afdaf574a37e
jeanneinchauspe <jeanne.inchauspe@gmail.com>
2021-10-29 12:10:17 +0100

    changes to test turtle

--- L-Systems-Tree-Modelling/test_Turtle2D_Interpreter.py
+++ L-Systems-Tree-Modelling/test_Turtle2D_Interpreter.py
@@ -1,17 +1,18 @@
 import unittest
 import numpy as np
-from Turtle2D_interpretation import Turtle2D
-
+from Turtle2D_interpretation import *
+import cv2
 class Turtle2DTest(unittest.TestCase):
     """
     Tests the :class:`Turtle2D` class.
     """
     def test_show_tree(self):
         turtle=Turtle2D(p0=np.array([250,0]),o0=np.array([0,1]),std_d=10,std_delta=29.5)
-        next_position=turtle.next_position()
-        drawer=Tree_drawing_2D(turtle,(100,100),color_scheme='natural',color_type='sequential')
+        drawer=Tree_drawing_2D(turtle,(500,500),2,3,color_scheme='natural',color_type='sequential')
+    
         test_string='[F[+F][-F[-F]F]F[+F][-F]]'
         drawer.draw_tree(test_string)
         with self.assertWarns(Warning):
             drawer.show_tree()
+            cv2.destroyAllWindows()
 
